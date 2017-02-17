706cb60a342239b86e8ef066e5cbedd71cd6842f
Krista Charner <kristacharner@outlook.com>
2017-02-17 14:15:40 -0500

    added Tim's updates to testimonials js code, as well as for stacking elements on patient's page

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -3,34 +3,65 @@ $(document).ready(function(){
 	//click function targets tags on the testimonlial page
 	$(document).on("click", ".down-arrow", function(){
 
-		var state = $(this).attr("data-state");
-		var testTag= $(this).parent();
+		var parent= $(this).parent();
+		parent.children(".collapse-text").slideToggle(500);
 
-		if(state == "inactive"){
-			state = $(this).attr("data-state", "active");//changes state to active
+		
+	});
+	
 
-			//slides the testimonial down
-	        testTag.children(".testimonial-text").slideToggle(500);
+	//click function targets tags on the testimonlial page
+	$(document).on("click", ".patients-arrow", function(){
 
-	        //makes image full sized
-	        testTag.children(".testImg")
-	        .css("transition", "all 1.5s ease")
-	        .css("-webkit-clip-path", "circle(109% at 50%)")
-	     
+		var parent= $(this).closest(".container");
+		parent.children(".collapse-text").slideToggle(500);
 
-		 } else if (state == "active"){
-			state = $(this).attr("data-state", "inactive");//changes state to inactive
-
-			//slides the testimonial down
-	        testTag.children(".testimonial-text").slideToggle(500);
-
-	        //returns image to circle
-	        testTag.children(".testImg")
-	        .css("-webkit-clip-path", "circle(30.0% at 50% 35%)")
-	        .css("transition", "all 1.s ease");
-		}
-			
+		
 	});
 
+
 });
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+
+// 	//click function targets tags on the testimonlial page
+// 	$(document).on("click", ".down-arrow", function(){
+
+// 		var state = $(this).attr("data-state");
+// 		var testTag= $(this).parent();
+
+// 		if(state == "inactive"){
+// 			state = $(this).attr("data-state", "active");//changes state to active
+
+// 			//slides the testimonial down
+// 	        testTag.children(".testimonial-text").slideToggle(500);
+
+// 	        //makes image full sized
+// 	        testTag.children(".testImg")
+// 	        .css("transition", "all 1.5s ease")
+// 	        .css("-webkit-clip-path", "circle(109% at 50%)")
+	     
+
+// 		 } else if (state == "active"){
+// 			state = $(this).attr("data-state", "inactive");//changes state to inactive
+
+// 			//slides the testimonial down
+// 	        testTag.children(".testimonial-text").slideToggle(500);
+
+// 	        //returns image to circle
+// 	        testTag.children(".testImg")
+// 	        .css("-webkit-clip-path", "circle(30.0% at 50% 35%)")
+// 	        .css("transition", "all 1.s ease");
+// 		}
+			
+// 	});
+
+// });
 
